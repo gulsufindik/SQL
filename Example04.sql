b3ab1d43b6db6f00ff9b1ffc6af3b906a7b3c3c4
gulsufindik <findik.gulsu@gmail.com>
2023-03-26 22:54:58 +0300

    DISTINCT-COUNT-ILIKE

---
+++ Example04.sql
@@ -0,0 +1,13 @@
+--DVD Rental Sample Database--
+SELECT DISTINCT replacement_cost FROM film;
+
+SELECT COUNT(DISTINCT replacement_cost) FROM film;
+
+SELECT COUNT(title) FROM film
+WHERE title LIKE 'T%' AND rating = 'G';
+
+SELECT COUNT(country) FROM country
+WHERE LENGTH(country) = 5;
+
+SELECT COUNT(city) FROM city
+WHERE city ILIKE 'r%';
