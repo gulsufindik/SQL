de79d2a294422a7fc3c1955bb636dc21fcdb0280
gulsufindik <findik.gulsu@gmail.com>
2023-03-27 22:41:08 +0300

    AVG-MAX-COUNT-DISTINCT

---
+++ Example06.sql
@@ -0,0 +1,14 @@
+--DVD Rental Sample Database--
+SELECT ROUND(AVG(rental_rate), 3) FROM film;
+
+SELECT COUNT(*) FROM film
+WHERE title LIKE 'C%';
+
+SELECT MAX(length), COUNT(rental_rate) FROM film
+WHERE rental_rate = 0.99;
+
+SELECT COUNT(DISTINCT replacement_cost) FROM film
+WHERE length > 150;
+
+
+
