2ec0034eee8706ef0739f3d43b6eda87e38da529
gulsufindik <findik.gulsu@gmail.com>
2023-03-27 14:50:14 +0300

    ASC-DESC-LIKE

---
+++ ORDER BY.sql
@@ -0,0 +1,20 @@
+--DVD Rental Sample Database--
+SELECT * FROM film
+ORDER BY title;
+
+SELECT * FROM film
+ORDER BY title DESC;
+
+SELECT * FROM film
+ORDER BY length;
+
+SELECT * FROM film
+ORDER BY length DESC;
+
+SELECT title, rental_rate, length FROM film
+WHERE title LIKE 'A%'
+ORDER BY rental_rate ASC, length DESC;
+
+SELECT * FROM film
+WHERE rating = 'G'
+ORDER BY length DESC;
