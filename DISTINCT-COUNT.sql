6ff7bd91d10e1792b8cb39f858aea1935ef62699
Gülsu Fındık <findik.gulsu@gmail.com>
2023-10-11 16:54:11 +0300

    Update DISTINCT-COUNT.sql

--- DISTINCT-COUNT.sql
+++ DISTINCT-COUNT.sql
@@ -1,7 +1,10 @@
 --DVD Rental Sample Database--
+-- "film" tablosundan benzersiz (distinct) kiralama ücretlerini seç
 SELECT DISTINCT rental_rate FROM film;
 
+-- "actor" tablosundan ilk ismi "A" ile başlayan aktörlerin sayısını seç
 SELECT COUNT(*) FROM actor
 WHERE first_name LIKE 'A%';
 
+-- "actor" tablosundan benzersiz (distinct) ilk isimlere sahip aktörlerin sayısını seç
 SELECT COUNT(DISTINCT first_name) FROM actor;
