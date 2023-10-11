7de2a35b88171c8b3022ee1f8c1d78200a700f85
Gülsu Fındık <findik.gulsu@gmail.com>
2023-10-11 17:06:28 +0300

    Update ORDER BY.sql

--- ORDER BY.sql
+++ ORDER BY.sql
@@ -1,20 +1,26 @@
 --DVD Rental Sample Database--
+-- "film" tablosunu başlığa göre sırala (artan sırada)
 SELECT * FROM film
 ORDER BY title;
 
+-- "film" tablosunu başlığa göre sırala (azalan sırada)
 SELECT * FROM film
 ORDER BY title DESC;
 
+-- "film" tablosunu uzunluğa göre sırala (artan sırada)
 SELECT * FROM film
 ORDER BY length;
 
+-- "film" tablosunu uzunluğa göre sırala (azalan sırada)
 SELECT * FROM film
 ORDER BY length DESC;
 
+-- Başlığı 'A' harfi ile başlayan filmleri kira ücretine göre artan, uzunluğa göre azalan sırada sırala
 SELECT title, rental_rate, length FROM film
 WHERE title LIKE 'A%'
 ORDER BY rental_rate ASC, length DESC;
 
+-- Derecesi 'G' olan filmleri uzunluğa göre azalan sırada sırala
 SELECT * FROM film
 WHERE rating = 'G'
 ORDER BY length DESC;
