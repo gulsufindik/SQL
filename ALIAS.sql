0183e1ad6a06591deb5c8a526470690c2f8df48c
Gülsu Fındık <findik.gulsu@gmail.com>
2023-10-11 16:51:38 +0300

    Update ALIAS.sql

--- ALIAS.sql
+++ ALIAS.sql
@@ -1,14 +1,21 @@
 --DVD Rental Sample Database--
+-- Tüm sütunları "actor" tablosundan seç
 SELECT * FROM actor;
 
+-- "first_name" sütununu "isim" olarak, "last_name" sütununu "soyisim" olarak seç
 SELECT first_name AS isim, last_name AS soyisim FROM actor;
 
+-- "first_name" sütununu "isim" olarak, "last_name" sütununu "soyisim" olarak seç
 SELECT first_name isim, last_name soyisim FROM actor;
 
+-- "first_name" sütununu "isim test" olarak, "last_name" sütununu "soyisim test" olarak seç
 SELECT first_name "isim test", last_name "soyisim test" FROM actor;
 
+-- "actor" tablosundaki kayıt sayısını seç
 SELECT COUNT(*) FROM actor;
 
+-- "actor" tablosundaki kayıt sayısını "aktor sayisi" olarak seç
 SELECT COUNT(*) AS "aktor sayisi" FROM actor;
 
+-- "first_name" ve "last_name" sütunlarını birleştirerek "İsim ve Soyisim" olarak seç
 SELECT CONCAT(first_name,' ', last_name) AS "İsim ve Soyisim" FROM actor;
